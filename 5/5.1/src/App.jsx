5da684de3127261ea8f6bd2f13b0fd5d898b00fa
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2023-11-26 11:13:58 +0200

    5.1 buttons

---
+++ 5/5.1/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Button from "./Components/Button";
+
+function App() {
+  return (
+    <>
+      <Button title="Important" boxId="box1" />
+      <Button title="Not Important" boxId="box2" />
+    </>
+  );
+}
+
+export default App;
